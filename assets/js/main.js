086f6cf106195e4562a236caaf4e20d13409ae54
Christian <christianrssdev@gmail.com>
2024-05-18 11:44:28 -0300

    Ajustes

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,20 +6,21 @@ class Site {
     }
 
     header() {
-        //let header = document.getElementById("topo");
-        //header.style.marginTop = "20px";
     }
 
     main() {
+        /* 
+            Adiciona margem automática ao topo do conteúdo principal.
+            Para que o conteúdo da página não fique sobreposto pelo topo fixado.
+        */
         let main = document.getElementById("conteudo");
-        main.style.paddingTop = document.getElementById("topo").offsetHeight + "px";
+        main.style.marginTop = document.getElementById("topo").offsetHeight + "px";
     }
 }
 
+/** Utilizei OOP porque é muito fácil o código ficar bagunçado em JavaScript */
 let site = new Site();
 
 window.addEventListener("load", (event) => {
-    /** Utilizei OOP porque é muito fácil o código ficar bagunçado em JavaScript */
-    
 
 });
